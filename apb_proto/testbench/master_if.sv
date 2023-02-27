7868ea564e51a32ab5a8a9284e49d163b6085d7c
wuqlan111 <wuqlan111@163.com>
2023-02-28 00:09:09 +0800

    Modify  master interface.

--- apb_proto/testbench/master_if.sv
+++ apb_proto/testbench/master_if.sv
@@ -11,37 +11,50 @@
 *********************************************************************************************************************************/
 
 
-`ifndef  _INCL_MASTER_IF
-`define  _INCL_MASTER_IF
+`ifndef  MASTER_IF_SV
+`define  MASTER_IF_SV
 
 `include "definition.sv"
 
 interface master_if;
     
-    bit [`APB_ADDR_WIDTH-1:0] addr;
-    bit clk;
-    bit master_error;   
-    bit other_error;    
-    bit [2:0] prot;    
-    bit ready;
-    bit [`APB_DATA_WIDTH-1:0] rdata;
-    bit [$clog2(`APB_SLAVE_DEVICES) :0] sels;
-    bit [(`APB_DATA_WIDTH / 8) -1:0] strb;
-    bit [`APB_DATA_WIDTH-1:0] wdata;
-    bit write;
-    bit valid;
+    bit  [`APB_ADDR_WIDTH-1:0]  addr;
+    bit  clk;
+    bit  master_error;
+    bit  other_error;
+    bit  ready;
+    bit  [`APB_DATA_WIDTH-1:0]  rdata;
+    bit  sel;
+    bit  [`APB_DATA_WIDTH-1:0]  wdata;
+    bit  write;
 
-    clocking sb @(negedge clk);
+    `ifdef  APB_PROT
+        bit  [2:0] prot;
+    `endif
+    `ifdef  APB_WSTRB
+        bit  [(`APB_DATA_WIDTH / 8) -1:0] strb;
+    `endif
+
+    clocking cb @(negedge clk);
         input  master_error, ready, rdata;
-        output addr, other_error, prot, sels, strb, wdata, write, valid;
+        output addr, other_error,
+                `ifdef APB_PROT
+                    prot,
+                `endif
+
+                `ifdef  APB_WSTRB
+                    strb, 
+                `endif
+                
+                sel, wdata, write;
     endclocking
 
-    modport TSB_MASTER (clocking sb, input clk);
+    modport TSB_MASTER_IF (clocking cb, input clk);
 
 endinterface //master_if
 
 
-typedef virtual master_if.TSB_MASTER  VTSB_MASTER_T;
+typedef virtual master_if.TSB_MASTER_IF  VTSB_MASTER_IF;
 
 
 `endif
