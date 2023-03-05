40050b718f0aa7b3c35b83b623f8d4387f5b196c
wuqlan111 <wuqlan111@163.com>
2023-03-05 20:50:58 +0800

    Modify apb monitor.

--- apb_proto/testbench/apb_monitor.sv
+++ apb_proto/testbench/apb_monitor.sv
@@ -117,7 +117,7 @@ task  apb_monitor::master_collect_pkt(apb_transaction tr);
     @(posedge  m_vif.clk);
 
     tr.rdata    =   !m_vif.write  && !m_vif.master_error ? m_vif.rdata:  0;
-    tr.error    =   m_vif.master_error || m_vif.master_error;
+    tr.error    =   (m_vif.other_error && !m_vif.ready) || m_vif.master_error;
 
     repeat(6) @(posedge  m_vif.clk);
 
