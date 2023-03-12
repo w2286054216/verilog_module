631a95a37e7a2e6e739221be690e862599d8fe04
wuqlan111 <wuqlan111@163.com>
2023-03-12 09:37:37 +0800

    Modify ahb_slave_transaction.

--- ahb_proto/testbench/ahb_slave_transaction.sv
+++ ahb_proto/testbench/ahb_slave_transaction.sv
@@ -23,14 +23,17 @@ class  ahb_slave_transaction extends uvm_sequence_item;
     rand  bit  [2: 0]  other_error;  
     rand  bit  [`AHB_DATA_WIDTH-1:0]  rdata[];
     rand  bit  [1: 0]  ready;
+    rand  bit  [3: 0]  size;    
 
-    constraint rdata_range { rdata[`AHB_ADDR_WIDTH-1:12] == 16'h8030;}
-    constraint error_ready { other_error <= (ready + 1) ;}
+    constraint  rdata_range  { foreach(rdata[i]) rdata[i][15:0] == 16'h8030; }
+    constraint  rdata_size   { rdata.size == 16; }
+    constraint  error_ready  { other_error <= (ready + 1) ;}
 
     `uvm_object_utils_begin(ahb_slave_transaction)
         `uvm_field_int(other_error, UVM_ALL_ON)
         `uvm_field_array_int(rdata, UVM_ALL_ON)
         `uvm_field_int(ready, UVM_ALL_ON)
+        `uvm_field_int(size, UVM_ALL_ON)
     `uvm_object_utils_end
 
 
