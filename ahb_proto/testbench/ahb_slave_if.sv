ec7fc81e9fdf2dfaab5e4079ec663a18f13f3a37
wuqlan111 <wuqlan111@163.com>
2023-03-12 11:53:26 +0800

    Modify  ahb_slave_if.

--- ahb_proto/testbench/ahb_slave_if.sv
+++ ahb_proto/testbench/ahb_slave_if.sv
@@ -1,49 +1,43 @@
 
-/**********************************************************************************************************************************
-* File Name:     slave_if.sv
+/*******************************************************************************
+* File Name:     ahb_slave_if.sv
 * Author:          wuqlan
 * Email:           
 * Date Created:    2022/12/28
-* Description:      Connect testbench to  APB slave interface.
+* Description:      Connect testbench to  AHB slave interface.
 *
 *
 * Version:         0.1
-*********************************************************************************************************************************/
+***********************************************************************************/
 
 
-`ifndef  SLAVE_IF_SV
-`define  SLAVE_IF_SV
+`ifndef  AHB_SLAVE_IF_SV
+`define  AHB_SLAVE_IF_SV
 
 `include "definition.sv"
 
 interface ahb_slave_if;
     
-    bit [`AHB_ADDR_WIDTH-1:0] addr;
-    bit clk;
-    bit other_error;    
-    bit [3:0] prot;    
-    bit ready;
-    bit slave_ready;
-    bit [`AHB_DATA_WIDTH-1:0] rdata;
-    bit sel;
-    bit slave_error;    
-    bit [(`AHB_DATA_WIDTH / 8) -1:0] strb;
-    bit [`AHB_DATA_WIDTH-1:0] wdata;
-    bit write;
+    bit  [`AHB_ADDR_WIDTH-1:0] addr;
+    bit  clk;
+    bit  other_error;
+    bit  ready;
+    bit  [`AHB_DATA_WIDTH-1:0]  rdata;
+    bit  sel;
+    bit  slave_error;
+
+    `ifdef  AHB_PROT
+        bit  [3:0]  prot;
+    `endif
+    `ifdef  AHB_WSTRB
+        bit  [(`AHB_DATA_WIDTH / 8) -1:0]  strb;
+    `endif
+    
+    bit  [`AHB_DATA_WIDTH-1:0]  wdata;
+    bit  write;
 
 
-
-    clocking cb @(negedge clk);
-        input  addr, slave_error, slave_ready, prot, sel, strb, wdata, write;
-        output  other_error, ready, rdata;
-    endclocking
-
-    modport TSB_SLAVE (clocking cb, input clk);
-
-
-endinterface //master_if
-
-typedef virtual slave_if.TSB_SLAVE  VTSB_SLAVE_T;
-
+endinterface //ahb_slave_if
 
 `endif
+
