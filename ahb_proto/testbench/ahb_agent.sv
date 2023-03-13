fe83f61a8a8f8a78ae5d5eb0ef167894c09d4ecd
wuqlan111 <wuqlan111@163.com>
2023-03-13 20:30:18 +0800

    Modify ahb_agent.

--- ahb_proto/testbench/ahb_agent.sv
+++ ahb_proto/testbench/ahb_agent.sv
@@ -21,20 +21,20 @@
 import uvm_pkg::*;
 
 
-
 class ahb_agent extends uvm_agent;
 
    `uvm_component_utils(ahb_agent)
     
-    my_sequencer  sqr;
-    my_driver     drv;
-    my_monitor    mon;
+    ahb_sequencer  sqr;
+    ahb_driver     drv;
+    ahb_monitor    mon;
+    bit  m_agt;
 
-   uvm_analysis_port #(my_transaction)  ap;
+   uvm_analysis_port #(ahb_transaction)  ap;
    
    function new(string name, uvm_component parent);
       super.new(name, parent);
-      `uvm_info("my_agent", "new is called", UVM_HIGH);
+      `uvm_info("ahb_agent", "new is called", UVM_HIGH);
    endfunction 
    
    extern virtual function void build_phase(uvm_phase phase);
@@ -50,11 +50,12 @@ class ahb_agent extends uvm_agent;
     function  void  build_phase(uvm_phase phase);
         super.build_phase(phase);
         if (is_active == UVM_ACTIVE) begin
-            sqr = my_sequencer::type_id::create("sqr", this);
-            drv = my_driver::type_id::create("drv", this);
+            sqr = ahb_sequencer::type_id::create("sqr", this);
+            drv = ahb_driver::type_id::create("drv", this);
         end
-        mon = my_monitor::type_id::create("mon", this);
-        `uvm_info("my_agent", "build_phase is called", UVM_HIGH);
+        mon = ahb_monitor::type_id::create("mon", this);
+        mon.mon_master  =  m_agt;
+        `uvm_info("ahb_agent", "build_phase is called", UVM_HIGH);
     endfunction 
 
 
