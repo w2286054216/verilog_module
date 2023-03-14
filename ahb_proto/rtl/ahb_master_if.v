22e8a9e4b26ff4b8ce8d67e14c45851c2cfce178
wuqlan111 <wuqlan111@163.com>
2023-03-14 23:44:49 +0800

    Modify ahb_master_if  FSM.

--- ahb_proto/rtl/ahb_master_if.v
+++ ahb_proto/rtl/ahb_master_if.v
@@ -209,43 +209,44 @@ always @(*) begin
             end
 
             STATE_TRANS_BUSY: begin
-                if ( !ahb_burst_out  || ( (ahb_burst_out != AHB_BURST_INCR) && ( trans_changed || !other_sel_in || 
-                        !other_valid_in || !burst_counter ) ) || other_error_in || ahb_resp_in )
+                if ( !ahb_burst_out || (!other_sel_in && trans_unready) || ( (ahb_burst_out != AHB_BURST_INCR) && 
+                        ( trans_changed || !other_sel_in || !other_valid_in || !burst_counter ) ) 
+                        || other_error_in || ahb_resp_in )
                     next_state          =    STATE_ERROR;
+                else  if(!other_sel_in)
+                    next_state          =    STATE_RST;
+                else  if (!other_valid_in)
+                    next_state          =    STATE_TRANS_IDLE;
                 else  if ( other_delay_in )
                     next_state          =    STATE_TRANS_BUSY;
-                else  if ( (ahb_burst_out != AHB_BURST_INCR ) || (other_valid_in && !trans_changed) || trans_unready )
+                else  if ( (ahb_burst_out != AHB_BURST_INCR ) || !trans_changed )
                     next_state          =    STATE_TRANS_SEQ;
-                else  if( !other_valid_in)
-                    next_state          =    STATE_RST;
-                else  if(trans_changed)
-                    next_state          =    STATE_TRANS_NONSEQ;
                 else
-                    next_state          =    STATE_TRANS_IDLE;
+                    next_state          =    STATE_TRANS_NONSEQ;
             end
 
             STATE_TRANS_NONSEQ: begin
-                if (  ( !other_valid_in && ( ahb_burst_out > AHB_BURST_INCR)) ||  other_error_in || 
-                            (!ahb_burst_out && other_delay_in ) ||  !other_sel_in )
+                if (!other_sel_in || ( !other_valid_in && ( ahb_burst_out > AHB_BURST_INCR)) ||  other_error_in || 
+                            (!ahb_burst_out && other_delay_in ) )
                     next_state              =     STATE_ERROR;
-                else if ( !other_valid_in && !ahb_burst_out )
+                else  if (!other_valid_in)
                     next_state              =     STATE_TRANS_IDLE;
-                else if (other_delay_in || !other_valid_in )
+                else  if (other_delay_in)
                     next_state              =     STATE_TRANS_BUSY;
-                else if (ahb_burst_out)
+                else  if (ahb_burst_out)
                     next_state              =     STATE_TRANS_SEQ;
-                else 
+                else
                     next_state              =     STATE_TRANS_NONSEQ;
             end
 
             STATE_TRANS_SEQ: begin
-                if ( !other_sel_in || !ahb_burst_out ||  other_error_in || ahb_resp_in || 
+                if ( !other_sel_in || !ahb_burst_out ||  other_error_in || ( ahb_resp_in && !ahb_ready_in) || 
                     ( burst_counter && (ahb_burst_out > AHB_BURST_INCR) && ( trans_changed || !other_valid_in ) ) 
                     || ( !burst_counter && (ahb_burst_out > AHB_BURST_INCR) && other_delay_in ) )
                     next_state         =     STATE_ERROR;
-                else if ( !burst_counter && !other_valid_in)
+                else  if (!other_valid_in)
                     next_state         =     STATE_TRANS_IDLE;
-                else if ( other_delay_in ||  !other_valid_in)
+                else if ( other_delay_in)
                     next_state         =     STATE_TRANS_BUSY;
                 else if ( !burst_counter )
                     next_state         =     STATE_TRANS_NONSEQ;
@@ -253,7 +254,6 @@ always @(*) begin
                     next_state         =    STATE_TRANS_SEQ;
                     
             end
-
 
             default: next_state           =   STATE_RST;
         
